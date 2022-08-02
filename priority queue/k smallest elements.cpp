d7f799d85e5eac4010725fc55a0aefaba8c713e5
aniket dnyaneshwar more <aniketmore27507@gmail.com>
2022-08-02 15:46:49 +0530

    k smallest elements

---
+++ priority queue/k smallest elements.cpp
@@ -0,0 +1,82 @@
+/*You are given with an integer k and an array of integers that contain numbers in random order. Write a program to find k smallest numbers from given array. You need to save them in an array and return it.
+Time complexity should be O(n * logk) and space complexity should not be more than O(k).
+Note: Order of elements in the output is not important.
+Input Format :
+The first line of input contains an integer, that denotes the value of the size of the array. Let us denote it with the symbol N.
+The following line contains N space separated integers, that denote the value of the elements of the array.
+The following line contains an integer, that denotes the value of k.
+Output Format :
+The first and only line of output print k smallest elements. The elements in the output are separated by a single space. 
+Constraints:
+Time Limit: 1 sec
+Sample Input 1 :
+13
+2 12 9 16 10 5 3 20 25 11 1 8 6 
+4
+Sample Output 1 :
+1 2 3 5 */
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+
+int main() {
+    int size;
+    cin >> size;
+
+    int *input = new int[size];
+
+    for (int i = 0; i < size; i++) {
+        cin >> input[i];
+    }
+
+    int k;
+    cin >> k;
+
+    vector<int> output = kSmallest(input, size, k);
+    sort(output.begin(), output.end());
+
+    for (int i = 0; i < output.size(); i++) {
+        cout << output[i] << " ";
+    }
+
+    delete[] input;
+}
+#include <queue>
+#include<vector>
+
+vector<int> kSmallest(int input[], int n, int k) {
+	// Write your code here
+	priority_queue <int> pq; 
+    
+    // push first k eements in the priority queue
+    for(int i=0 ;i<k; i++)
+       pq.push(input[i]);
+        
+    // for reaining elements compare the element with max of pq ie top
+    //if smaller push in heap
+    for(int i =k ;i<n ; i++)
+    {
+        if(input[i] < pq.top())
+        {
+            pq.pop();
+            pq.push(input[i]);
+        }
+    }
+    
+    // cnvert heap into vector and return vector
+    vector<int> ans;
+    
+    while (!pq.empty())
+    {
+        ans.push_back(pq.top());
+        pq.pop();
+    }
+    
+    return ans;
+    
+}
